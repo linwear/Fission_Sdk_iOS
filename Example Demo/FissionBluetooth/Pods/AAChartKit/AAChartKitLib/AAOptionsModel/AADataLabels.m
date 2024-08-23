036ff8d008aca2e359a782c2c660c62b45d361f2
wsr1949 <921903719@qq.com>
2024-08-23 13:54:38 +0800

    Update screenshot

--- Example Demo/FissionBluetooth/Pods/AAChartKit/AAChartKitLib/AAOptionsModel/AADataLabels.m
+++ Example Demo/FissionBluetooth/Pods/AAChartKit/AAChartKitLib/AAOptionsModel/AADataLabels.m
@@ -33,6 +33,16 @@
 #import "AADataLabels.h"
 #import "NSString+toPureJSString.h"
 
+AAChartDataLabelsShapeType const AAChartDataLabelsShapeTypeSquare   = @"square";
+AAChartDataLabelsShapeType const AAChartDataLabelsShapeTypeCallout  = @"callout";
+AAChartDataLabelsShapeType const AAChartDataLabelsShapeTypeCircle   = @"circle";
+AAChartDataLabelsShapeType const AAChartDataLabelsShapeTypeDiamond  = @"diamond";
+AAChartDataLabelsShapeType const AAChartDataLabelsShapeTypeTriangle = @"triangle";
+
+AAChartDataLabelsOverflowType const AAChartDataLabelsOverflowTypeNone    = @"none";
+AAChartDataLabelsOverflowType const AAChartDataLabelsOverflowTypeAllow   = @"allow";
+AAChartDataLabelsOverflowType const AAChartDataLabelsOverflowTypeJustify = @"justify";
+
 @implementation AADataLabels
 
 - (instancetype)init {
@@ -61,10 +71,10 @@ AAPropSetFuncImplementation(AADataLabels, NSString *, backgroundColor)
 AAPropSetFuncImplementation(AADataLabels, NSString *, borderColor)
 AAPropSetFuncImplementation(AADataLabels, NSNumber *, borderRadius)
 AAPropSetFuncImplementation(AADataLabels, NSNumber *, borderWidth)
-AAPropSetFuncImplementation(AADataLabels, NSString *, shape)
+AAPropSetFuncImplementation(AADataLabels, AAChartDataLabelsShapeType, shape)
 AAPropSetFuncImplementation(AADataLabels, BOOL      , crop)
 AAPropSetFuncImplementation(AADataLabels, BOOL      , inside)
-AAPropSetFuncImplementation(AADataLabels, NSString *, overflow)
+AAPropSetFuncImplementation(AADataLabels, AAChartDataLabelsOverflowType, overflow)
 AAPropSetFuncImplementation(AADataLabels, BOOL      , softConnector)//Whether to render the connector as a soft arc or a line with sharp break. Only useful for pie, pyramid and funnel chart
 AAPropSetFuncImplementation(AADataLabels, NSDictionary *, textPath)
 AAPropSetFuncImplementation(AADataLabels, NSDictionary *, filter);
--- Example Demo/FissionBluetooth/Pods/Target Support Files/BRPickerView/BRPickerView-umbrella.h
+++ Example Demo/FissionBluetooth/Pods/Target Support Files/BRPickerView/BRPickerView-umbrella.h
@@ -11,17 +11,20 @@
 #endif
 
 #import "BRPickerView.h"
-#import "BRAddressModel.h"
-#import "BRAddressPickerView.h"
-#import "BRBaseView.h"
+#import "BRPickerAlertView.h"
 #import "BRPickerStyle.h"
 #import "BRPickerViewMacro.h"
 #import "NSBundle+BRPickerView.h"
 #import "BRDatePickerView+BR.h"
 #import "BRDatePickerView.h"
 #import "NSDate+BRPickerView.h"
+#import "BRAddressModel.h"
+#import "BRAddressPickerView.h"
+#import "BRBaseView.h"
 #import "BRResultModel.h"
 #import "BRStringPickerView.h"
+#import "BRTextModel.h"
+#import "BRTextPickerView.h"
 
 FOUNDATION_EXPORT double BRPickerViewVersionNumber;
 FOUNDATION_EXPORT const unsigned char BRPickerViewVersionString[];
